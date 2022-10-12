0ec86bbbbfbc9f7950ef32f8230244073c9459f0
Vitaly Turovsky <vital2580@icloud.com>
2022-10-12 10:55:23 +0300

    improve builtin typings in project

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -2,3 +2,13 @@ declare module 'FIG_ALL_SPECS' {
     const specs: { default: Fig.Spec }[]
     export default specs
 }
+
+// make builtin methods usable with TypeScript
+
+type StringKeys<T extends object> = Extract<keyof T, string>
+
+interface ObjectConstructor {
+    keys<T extends object>(obj: T): StringKeys<T>[]
+    entries<T extends object>(obj: T): [StringKeys<T>, T[keyof T]][]
+    fromEntries<T extends [string, any][]>(obj: T): Record<T[number][0], T[number][1]>
+}
